45c6430bc20e39f5ac469efea1deb0eb676059b7
CuoreDiRoccia <marco.paschetto@yahoo.it>
2016-05-02 20:05:43 +0200

    disposizione delle pedine e correzione errori

--- dama.js
+++ dama.js
@@ -23,7 +23,7 @@ var pedina = function(riga, colonna) {
     
     this.nelQuadratoNero = function(){
         var nelQuadratoNero = false;
-        if ((this.colonna % 2 == 0 && this.riga % 2 == 0) ||(this.colonna % 2 != 0 && this.riga % 2 == 0)){
+        if ((this.colonna % 2 == 0 && this.riga % 2 == 0) ||(this.colonna % 2 != 0 && this.riga % 2 != 0)){
             nelQuadratoNero = true;
         }
         return nelQuadratoNero;
@@ -43,26 +43,47 @@ var pedina = function(riga, colonna) {
 };
 
 
+var pedina1B = new pedina(1,1);
+var pedina2B = new pedina(1,3);
+var pedina3B = new pedina(1,5);
+var pedina4B = new pedina(1,7);
+var pedina5B = new pedina(2,2);
+var pedina6B = new pedina(2,4);
+var pedina7B = new pedina(2,6);
+var pedina8B = new pedina(2,8);
+var pedina9B = new pedina(3,1);
+var pedina10B = new pedina(3,3);
+var pedina11B = new pedina(3,5);
+var pedina12B = new pedina(3,7);
+
+var pedina1N = new pedina(6,2);
+var pedina2N = new pedina(6,4);
+var pedina3N = new pedina(6,6);
+var pedina4N = new pedina(6,8);
+var pedina5N = new pedina(7,1);
+var pedina6N = new pedina(7,3);
+var pedina7N = new pedina(7,5);
+var pedina8N = new pedina(7,7);
+var pedina9N = new pedina(8,2);
+var pedina10N = new pedina(8,4);
+var pedina11N = new pedina(8,6);
+var pedina12N = new pedina(8,8);
+
+var pedine = [[],[]];
+
+pedine[0] = [pedina1B.locazione(),pedina2B.locazione(),pedina3B.locazione(),pedina4B.locazione(),
+    pedina5B.locazione(),pedina6B.locazione(),pedina7B.locazione(),pedina8B.locazione(),
+    pedina9B.locazione(),pedina10B.locazione(),pedina11B.locazione(),pedina12B.locazione()];
+
+pedine[1] = [pedina1N.locazione(),pedina2N.locazione(),pedina3N.locazione(),pedina4N.locazione(),
+    pedina5N.locazione(),pedina6N.locazione(),pedina7N.locazione(),pedina8N.locazione(),
+    pedina9N.locazione(),pedina10N.locazione(),pedina11N.locazione(),pedina12N.locazione()];
 
 
-var pedina1 = new pedina(4,4);
-
-
-//console.log(pedina1.locazione());
-
-pedina1.riga = 4;
-
-//console.log(pedina1.locazione());
 
 
 
-//console.log(pedina1.nellaScacchiera());
-
-//console.log(pedina1.nelQuadratoNero());
-console.log(pedina1.locazioneValida());
-
-
-
+console.log(pedine);
 
 
 
